16cf6be574e2865fed087925461455b2a38f161e
JayeRNeal <103981244+JayeRNeal@users.noreply.github.com>
2022-04-24 13:31:21 -0600

    VCS update

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ print("Hello World!")
 print('Version control systems are tools that teams use to change code over time. They track and manage changes to files over time. \
 Version control systems help teams work faster with them, then without them. As well as, allowing multiple developers, designers, \
 and team members to work on the same project. VCS makes insure that all team members have access to the latest codes, and \
-tracks the modifications that are made. \
+tracks the modifications that are made. To see the current directory that you are working in you must type pwd (print work directory).\
+You can change dirctories by pressing the cd command. For example, typing cd pictures will bring you to your pictures folder.\
 In closing Version Control Systems are a powerful tool that helps teams (developers, designers, etc.). VCS code improves\
 visibility, helps teams collaborate around the world, and accelerates product delivery. Teams are better because of it.')
